d76571c3c58054d16af460de74e4b34d281a90a2
Huong NV <huongnv@tohsoft.com>
2019-08-29 11:34:16 +0700

    Initial commit

---
+++ src/main/java/com/anysoftkeyboard/addon/PackBroadcastReceiverBase.java
@@ -0,0 +1,10 @@
+package com.anysoftkeyboard.addon;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class PackBroadcastReceiverBase extends BroadcastReceiver {
+    public void onReceive(Context context, Intent intent) {
+    }
+}
